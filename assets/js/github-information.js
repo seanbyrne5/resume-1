831d70d0fe0dbb371dec59ddde22442b9787974b
seanbyrne5 <byrner14@hotmail.com>
2024-06-23 16:55:21 +0100

    reverting to base resume with minor changes

---
+++ assets/js/github-information.js
@@ -0,0 +1,91 @@
+function userInformationHTML(user) {
+    // Use the login name if the user's name is null or empty
+    var displayName = user.name ? user.name : user.login;
+
+    return `
+        <h2>${displayName}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
+
+function fetchGitHubInformation(username) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
+    if (!username) {
+        username = $("#gh-username").val();
+    }
+
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+    ).then(
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
+}
+
+$(document).ready(function() {
+    $("#fetch-info").on("click", function() {
+        fetchGitHubInformation();
+    });
+    // Fetch default GitHub username information when the page loads
+    fetchGitHubInformation('seanbyrne5');
+});
